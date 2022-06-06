6b39226af47673fad24644a170e784b86654a6ea
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-05 21:21:19 -0700

    add

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -2,5 +2,10 @@ var A = 'time'
 var B= 'date'
 var C= A+B
 document.write(C)
+function addition_Function() {
+    var addition =2+2;
+    document.getElementById('Math').innerHTML ='2+2= '+addition;
+}
+
 
 
