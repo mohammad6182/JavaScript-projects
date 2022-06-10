4ae2b54f9720bf769f85017a53ea7f346417160c
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-09 21:01:51 -0700

    add

--- Basic_JavaScript_Projects/Project3_math_Operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_Operators/JS/main.js
@@ -1,5 +1,21 @@
 function multiplication() {
     var simple_Math = 8 * 6;
+    
     document.getElementById('Math').innerHTML = '8 * 6 = ' + simple_Math;
     
 }
+function addition() {
+    var addition_1 = 6+2;
+    document.getElementById('Math_1').innerHTML = '6 + 2 = '+addition_1;
+}
+
+function subtraction() {
+    var subtraction_1 = 14 - 2;
+    document.getElementById('Math_2').innerHTML = '14 - 2 ='+subtraction_1;
+}
+
+function dividend() {
+    var dividend_1 = 19 - 3;
+    document.getElementById('Math_3').innerHTML = '19 - 3 ='+dividend_1;
+}
+
