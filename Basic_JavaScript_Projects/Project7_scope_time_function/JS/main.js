25621105e971f75e4c1e41b16d9249253889e116
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-11 23:06:44 -0700

    add

---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,20 @@
+var A=15;
+function Deduct_number_1() {
+    document.write(25 - A );
+}
+function Deduct_number_2() {
+    document.write(A-1);
+}
+Deduct_number_1();
+Deduct_number_2();
+
+
+function Add_number_1() {
+var B= 14;
+document.write(1+B);
+}
+function Add_number_2() {
+document.write(B+3);
+}
+Add_number_1();
+Add_number_2();
