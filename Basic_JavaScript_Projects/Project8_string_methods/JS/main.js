00a442b7ea8cc0bf4310a3390251b20bd72e839e
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-13 00:06:05 -0700

    add

---
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,8 @@
+function complete_formula() {
+    var step_1 = 'first, clean the chicken and cut it in 3 inches peqces';
+    var step_2 = 'then, make some marinate juice wich includes:';
+    var step_3 = '1-union 2-lime juice 3-safferon 4-olive oil';
+    var step_4 = "leave the mix for 24 haours and then start barbq";
+    var full_formula = step_1.concat(step_2,step_3,step_4);
+    document.getElementById('concat').innerHTML= full_formula;
+}
