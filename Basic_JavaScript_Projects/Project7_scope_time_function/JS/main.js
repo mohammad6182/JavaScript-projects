d2fc92c8ef5c2ec7eb103f25867a58fa1d080607
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-12 23:43:35 -0700

    add

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -1,3 +1,21 @@
+function get_destination() {
+    if (new Date().getHours() < 16) {
+        document.getElementById('Traveling').innerHTML= "We have a destination for you";
+    }
+}
+
+function get_passcode() {
+    passcode = document.getElementById("passcode").value;
+    if (passcode=181) {
+        access ="Your passcode is right, enjoy your time at the club!"; 
+    }
+    else {
+        access = "you have entered wrong passcode, you have 1 more chance before we delete your account";
+
+    }
+    document.getElementById('what is your passcode?').innerHTML.value;
+}
+
 var A=15;
 function Deduct_number_1() {
     document.write(25 - A );
@@ -18,3 +36,4 @@ document.write(B+3);
 }
 Add_number_1();
 Add_number_2();
+
