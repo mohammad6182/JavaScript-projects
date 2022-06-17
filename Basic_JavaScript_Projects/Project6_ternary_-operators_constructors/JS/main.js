f1e8c856a7c2803ff692b604d4d615a9570158ec
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-16 19:07:10 -0700

    add

--- Basic_JavaScript_Projects/Project6_ternary_-operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary_-operators_constructors/JS/main.js
@@ -5,7 +5,19 @@ function Ride_Function() {
     document.getElementById('Ride').innerHTML = Can_ride + " to ride";
 }
 
-function Vehicle(Make, Model, Year, Color)//see page 132
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year=Year;
+    this.Vehicle_Color=Color;
+}
+var Jack=new Vehicle("Dodge", "Viper", "2020","Red");
+var Emily = new Vehicle ("Jeep", "Trail Hawk", "2019", "White and Black");
+var Erik =new Vehicle("Ford", "Pinot", "1971", "Mustang");
+function myFunction() {
+    document.getElementById('Keywords_and_Constructors'),innerHTML="Erik drives a "+ Erik.Vehicle_Color+Erik.Vehicle_Model+Erik.Vehicle_Year;
+}
+
 
 
 
