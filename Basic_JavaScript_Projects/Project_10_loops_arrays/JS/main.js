0b510d050a155496a8849ee024f82d04e9eac951
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-06-17 00:21:24 -0700

    add

--- Basic_JavaScript_Projects/Project_10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project_10_loops_arrays/JS/main.js
@@ -26,3 +26,24 @@ function array_Function() {
     array_1[3]="New York";
     document.getElementById('Array').innerHTML= array_1[1]+" is the rainiest city in northwest"+'.';
 }
+
+
+function constant_function() {
+    const Vehicle_choice={type:'SUV', Brand:"Mercedes", Color:"Black"};
+    Vehicle_choice.Color="White";
+    Vehicle_choice.Price="$130K";
+    document.getElementById('Constent').innerHTML="The " +
+Vehicle_choice.Price + Vehicle_choice.Brand + " is the ultimate Suv";
+
+}
+
+let Laptop = {  //this one is not working.//
+    Make: "Dell",
+    Model:"Inspiron",
+    Year: "2020",
+    CPU: "intel core i-7",
+    description : function() {
+        return "My New Laptop is a " + this.Make+this.Model+this.CPU;
+    }
+};
+document.getElementById('Laptop_Categories').innerHTML=Laptop.description();
